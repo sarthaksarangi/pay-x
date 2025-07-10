646c8e509dbde46bfd269336ac694b2e6a6ae46a
sarthaksarangi <sarthakrohan5@gmail.com>
2025-07-11 01:44:35 +0530

    added db schema changes

---
+++ apps/user-app/components/SidebarItem.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+
+export const SidebarItem = ({
+  href,
+  title,
+  icon,
+}: {
+  href: string;
+  title: string;
+  icon: React.ReactNode;
+}) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <div
+      className={`flex ${isActive ? "text-[#6a51a6]" : "text-slate-500"} cursor=pinter p-2  pl-8`}
+      onClick={() => router.push(href)}
+    >
+      <div className="pr-2">{icon}</div>
+      <div
+        className={`font-bold ${isActive ? "text-[#6a51a6]" : "text-slate-500"}`}
+      >
+        {title}
+      </div>
+    </div>
+  );
+};
